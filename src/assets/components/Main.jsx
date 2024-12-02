70469a9ce791c46c0b2f67289bbad57b521594eb
MarcoProstamo <Marcopros1201@gmail.com>
2024-12-02 14:23:13 +0100

    Add → Static Structure

--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,3 +1,24 @@
 export default function Main() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="container">
+        <section className="filtering-tags">
+          <button className="tag active">HTML</button>
+          <button className="tag">CSS</button>
+          <button className="tag">JS</button>
+          <button className="tag">4</button>
+        </section>
+        <section className="card-container">
+          <div className="card">
+            <div className="card-title">Lorem, ipsum dolor.</div>
+            <div>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Eligendi, magni odio. Quaerat unde earum dolorum laudantium.
+              Magnam saepe id ducimus.
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
 }
